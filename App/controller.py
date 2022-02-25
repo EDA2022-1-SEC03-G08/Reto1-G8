e477c54caf701d25105bb50b71f17dc091369539
d.molinad <d.molinad@uniandes.edu.co>
2022-02-25 11:49:33 -0500

    ActualizacionController

--- App/controller.py
+++ App/controller.py
@@ -19,83 +19,81 @@
  * You should have received a copy of the GNU General Public License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  """
-
 import config as cf
 import model
 import csv
-
+...
+csv.field_size_limit(2147483647)
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
 """
 
-# Inicialización del Catálogo de libros
 
+# Inicialización del Catálogo de libros
 def newController():
     """
     Crea una instancia del modelo
     """
-    control = {
-        'model': None
-    }
+    control = {'model': None}
     control['model'] = model.newCatalog()
     return control
 
-# Funciones para la carga de datos
 
+# Funciones para la carga de datos
 def loadData(control):
-    """
-    Carga los datos de los archivos y cargar los datos en la
-    estructura de datos
-    """
+
     catalog = control['model']
     albums = loadAlbums(catalog)
-    artists = loadArtists(catalog)
+    artists = loadArtist(catalog)
     tracks = loadTracks(catalog)
+    sortAlbums(catalog)
+    sortArtists(catalog)
     sortTracks(catalog)
+
     return albums, artists, tracks
 
 
 def loadAlbums(catalog):
-    """
-    Carga los albumes del archivo.  Por cada album se toman sus autores y por
-    cada uno de ellos, se crea en la lista de autores, a dicho autor y una
-    referencia al libro que se esta procesando.
-    """
-    albumsfile = cf.data_dir + 'Spotify/spotify-albums-utf8-5pct.csv'
-    input_file = csv.DictReader(open(albumsfile, encoding='utf-8'))
-    for album in input_file:
+
+    albumsfile = cf.data_dir + 'spotify-albums-utf8-20pct.csv'
+    input_file_al = csv.DictReader(open(albumsfile, encoding='utf-8'))
+    for album in input_file_al:
         model.addAlbum(catalog, album)
     return model.albumSize(catalog)
 
 
-def loadArtists(catalog):
-    """
-    Carga todos los artistas del archivo y los agrega a la lista de artistas
-    """
-    artistsfile = cf.data_dir + 'Spotify/spotify-artists-utf8-5pct.csv'
-    input_file = csv.DictReader(open(artistsfile, encoding='utf-8'))
-    for artist in input_file:
+def loadArtist(catalog):
+    artistsfile = cf.data_dir + 'spotify-artists-utf8-20pct.csv'
+    input_file_ar = csv.DictReader(open(artistsfile, encoding='utf-8'))
+    for artist in input_file_ar:
         model.addArtist(catalog, artist)
-    return model.artistsSize(catalog)
+    return model.artistSize(catalog)
 
 
 def loadTracks(catalog):
-    """
-    Carga la información que asocia tags con tracks.
-    """
-    tracksfile = cf.data_dir + 'Spotify/spotify-tracks-utf8-5pct.csv'
-    input_file = csv.DictReader(open(tracksfile, encoding='utf-8'))
-    for track in input_file:
+    tracksfile = cf.data_dir + 'spotify-tracks-utf8-20pct.csv'
+    input_file_tr = csv.DictReader(open(tracksfile, encoding='utf-8'))
+    for track in input_file_tr:
         model.addTrack(catalog, track)
-    return model.tracksSize(catalog)
+    return model.trackSize(catalog)
 
 
 # Funciones de ordenamiento
-def sortTracks(catalog):
-    """
-    Ordena los tracks por average_rating
-    """
-    model.sortTracks(catalog)
+def sortAlbums(catalog):
+    model.sortAlbums(catalog)
 
+
+def sortArtists(catalog):
+    model.sortArtists(catalog)
+
+
+def sortTracks(catalog):
+    model.sortTracks(catalog)
 # Funciones de consulta sobre el catálogo
+
+
+def albumesPorAnio(control, anio_o, anio_f):
+
+    albums = model.albumesPorAnio(control['model'], anio_o, anio_f)
+    return albums
